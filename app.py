84b695930d15d0685abbabc59874bca48cab8bfe
Luthfi Eko Trinowo <luthfii.hakiim@gmail.com>
2020-12-05 16:34:22 +0700

    add: form bar for webapp input

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ def home():
     return render_template('home.html')
     # return 'hello world'
 
+# @app.route('/predict')
+# def predict():
+#     return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
